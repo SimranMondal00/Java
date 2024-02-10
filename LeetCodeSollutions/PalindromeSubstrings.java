bd36c03e1e68007f2335e2bfccf5514656bde1ad
Simran Mondal <119064514+SimranMondal00@users.noreply.github.com>
2024-02-10 06:48:48 +0530

    Add files via upload

---
+++ LeetCodeSollutions/PalindromeSubstrings.java
@@ -0,0 +1,27 @@
+package LeetCodeSollutions;
+
+public class PalindromeSubstrings {
+    public static int countSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                if (isPalindrome(s.substring(i, j))) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private static boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
