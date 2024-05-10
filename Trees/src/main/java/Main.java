3def2a23dff6d3b29420f9874e324f8ffc134262
Simran Mondal <119064514+SimranMondal00@users.noreply.github.com>
2024-05-11 01:07:54 +0530

    Update Main.java

--- Trees/src/main/java/Main.java
+++ Trees/src/main/java/Main.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Binary Tree :- ");
-    Scanner sc = new Scanner(System.in);
-    BinaryTree tree = new BinaryTree();
-    tree.populate(sc);
+    // System.out.println("Binary Tree :- ");
+    // Scanner sc = new Scanner(System.in);
+    // BinaryTree tree = new BinaryTree();
+    // tree.populate(sc);
+    // tree.display();
+     BST tree = new BST();
+    int[] nums = {5,2,7,1,4,6,9,8,3,10};
+    tree.populate(nums);
     tree.display();
   }
 }
