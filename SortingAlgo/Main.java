ece8ddaff0f8929bce5d4f1eac71ee82cfe6d83a
Simran Mondal <119064514+SimranMondal00@users.noreply.github.com>
2024-08-12 08:19:27 +0530

    Count Sort

---
+++ SortingAlgo/Main.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+public class Main {
+  private static void countSort(int[] arr){
+    if(arr == null || arr.length <=1){
+      return;
+    }
+    int max = arr[0];
+    for(int num : arr){
+      if(num > max){
+        max = num;
+      }
+    }
+    int[] countArray = new int[max+1];
+    for(int num : arr){
+      countArray[num]++;
+    }
+    int index=0;
+    for(int i=0;i<=max;i++){
+      while(countArray[i] > 0){
+        arr[index]=i;
+        index++;
+        countArray[i]--;
+      }
+    }
+  }
+  public static void main(String[] args) {
+    int[] arr = {6,3,10,9,2,2,4,9,7};
+    countSortHash(arr);
+    System.out.println(Arrays.toString(arr));
+  }
+  private static void countSortHash(int[] arr){
+    if(arr == null || arr.length <= 1){
+      return;
+    }
+    int max = Arrays.stream(arr).max().getAsInt();
+    int min = Arrays.stream(arr).min().getAsInt();
+    Map<Integer, Integer> countMap = new HashMap<>();
+    for(int num: arr){
+      countMap.put(num,countMap.getOrDefault(num,0)+1);
+    }
+    int index=0;
+    for(int i=min;i<=max;i++){
+      int count = countMap.getOrDefault(i,0);
+      for(int j=0;j<count;j++){
+        arr[index]=i;
+        index++;
+      }
+    }
+  }
+}
